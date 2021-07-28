2cbaf0f28047e50ec0e12a30d2bfe83ee046434b
XavierAP <2506829+XavierAP@users.noreply.github.com>
2021-07-29 01:31:24 +0200

    JP.Utils.Xamarin

---
+++ JP.Utils.Xamarin/CenteredDialogPage.cs
@@ -0,0 +1,51 @@
+﻿using Xamarin.Forms;
+
+namespace JP.Utils
+{
+	public class CenteredDialogPage : ContentPage
+	{
+		readonly StackLayout layout;
+
+		public CenteredDialogPage()
+		{
+			layout = new StackLayout
+			{
+				HorizontalOptions = LayoutOptions.Center,
+			};
+			this.Content = new ScrollView
+			{
+				Content = layout,
+				HorizontalOptions = LayoutOptions.Center,
+			};
+		}
+
+		public Label
+		AddLabel(string text)
+			=> AddElement(new Label
+			{
+				Text = text
+			});
+
+		public Entry
+		AddEntry(string hintText, Keyboard keyboard)
+			=> AddElement(new Entry
+			{
+				Placeholder = hintText,
+				Keyboard = keyboard,
+			});
+
+		public Button
+		AddButton(string text)
+			=> AddElement(new Button
+			{
+				Text = text,
+			});
+
+		private T AddElement<T>(T elem)
+			where T : View
+		{
+			layout.Children.Add(elem);
+			return elem;
+		}
+	}
+}
---
+++ JP.Utils.Xamarin/TextViewUnion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace JP.Utils
+{
+	/// <summary>Union-like type that wraps either an <see cref="Entry"/> or a <see cref="Label"/>.</summary>
+	public class TextViewUnion
+	{
+		public string Text
+		{
+			get => Entry?.Text ??
+				   Label?.Text ?? throw new InvalidProgramException();
+
+			set { if(Entry != null) Entry.Text = value; }
+		}
+
+		public Entry? Entry { get; }
+		public Label? Label { get; }
+
+		public TextViewUnion(Entry payload) => Entry = payload;
+		public TextViewUnion(Label payload) => Label = payload;
+	}
+}
