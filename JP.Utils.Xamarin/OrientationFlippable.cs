ffa7f2718cbbf2d8cf903143f0ff7fa119df64f5
XavierAP <2506829+XavierAP@users.noreply.github.com>
2021-08-13 19:16:07 +0200

    OrientationFlipBehavior

---
+++ JP.Utils.Xamarin/OrientationFlippable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace JP.Utils
+{
+	public enum Orientation
+	{
+		NotSet,
+		Portrait,
+		Landscape
+	}
+
+	public class OrientationFlipBehavior
+	{
+		readonly VisualElement element;
+
+		public event Action<Orientation>? Flipped;
+
+		public OrientationFlipBehavior(VisualElement element)
+		{
+			this.element = element;
+			element.SizeChanged += OnSizeChanged;
+		}
+		
+
+		public Orientation PageOrientation
+		{
+			get => _PageOrientation;
+
+			private set
+			{
+				if(value == _PageOrientation) return;
+				_PageOrientation = value;
+				Flipped?.Invoke(value);
+			}
+		}
+		private Orientation _PageOrientation = Orientation.NotSet;
+
+
+		private void OnSizeChanged(object sender, EventArgs ea)
+		{
+			if(element.Width > element.Height)
+				PageOrientation = Orientation.Landscape;
+			else
+				PageOrientation = Orientation.Portrait;
+		}
+	}
+}
