dc51a47608c65b038b511e6d8cca7533f24da5ec
XavierAP <2506829+XavierAP@users.noreply.github.com>
2022-01-23 21:02:15 +0100

    Not generic/reusable enough

--- JP.Utils.Xamarin/TablePage.cs
+++ JP.Utils.Xamarin/TablePage.cs
@@ -5,12 +5,9 @@ namespace JP.Utils
 {
 	public class TablePage : ContentPage
 	{
-		State current = State.Empty;
-		enum State { Empty, FillingTable, ButtonsSet }
-
 		int
-			nColumns,
 			iColumnCurrent = 0,
+			iColumnLast = 0,
 			nButtons = 0;
 		
 		readonly Grid
@@ -19,7 +16,7 @@ namespace JP.Utils
 		
 		readonly RowDefinition rowLayout = new RowDefinition { Height = GridLength.Auto };
 		
-		public bool IsEmpty => table.RowDefinitions.Count < 1;
+		public int RowCount => table.RowDefinitions.Count;
 
 		public TablePage()
 		{
@@ -27,31 +24,6 @@ namespace JP.Utils
 			wholeLayout.Children.Add(CreateTableScrollView(out table));
 			wholeLayout.Children.Add(buttonsLayout = CreateButtonsLayout());
 			this.Content = wholeLayout;
-		}
-
-		/// <summary>Call first. Optionally for no headers, call <see cref="SetColumns(int)"/> instead.</summary>
-		public void SetHeaders(params string[] headers)
-		{
-			SetColumns(headers.Length);
-			AddRow();
-			foreach(var h in headers)
-				AddCellToCurrentRow(new Label
-				{
-					Text = h,
-					FontAttributes = FontAttributes.Bold,
-				});
-		}
-
-		/// <summary>Alternative to <see cref="SetHeaders(string[])"/></summary>
-		public void SetColumns(int columnCount)
-		{
-			if(current > State.Empty) throw new InvalidOperationException("Columns already defined.");
-
-			current = State.FillingTable;
-			nColumns = columnCount;
-
-			for(int i = 0; i < columnCount; i++)
-				table.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
 		}
 
 		public void AddRow()
@@ -64,10 +36,8 @@ namespace JP.Utils
 		{
 			CheckTableSpaceWhenAddingCell(out var irow);
 			table.Children.Add(cellView, iColumnCurrent++, irow);
-			current = State.FillingTable;
 		}
 
-		/// <summary>Call last.</summary>
 		public Button AddButtonAtBottom(string text, EventHandler clicked)
 		{
 			var btn = new Button { Text = text };
@@ -76,16 +46,15 @@ namespace JP.Utils
 			buttonsLayout.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
 			buttonsLayout.Children.Add(btn, nButtons++, 0);
 			
-			current = State.ButtonsSet;
 			return btn;
 		}
 
 		public void DeleteLastRow()
 		{
-			if(IsEmpty) throw new InvalidOperationException("There are no rows to delete.");
+			if(RowCount < 1) throw new InvalidOperationException("There are no rows to delete.");
 
 			var lastIndex = table.Children.Count - 1;
-			var lastIndexToRemain = lastIndex - nColumns;
+			var lastIndexToRemain = lastIndex - iColumnLast - 1;
 			for(var i = lastIndex; i > lastIndexToRemain; --i)
 				table.Children.RemoveAt(i);
 
@@ -109,13 +78,15 @@ namespace JP.Utils
 
 		private void CheckTableSpaceWhenAddingCell(out int iRowCurrent)
 		{
-			if(current < State.FillingTable) throw new InvalidOperationException("Must set columns or headers before adding rows or cells.");
-			if(current > State.FillingTable) throw new InvalidOperationException("Cannot add cells after buttons.");
-
 			iRowCurrent = table.RowDefinitions.Count - 1;
 
-			if(iRowCurrent < 0) throw new InvalidOperationException("Must add a row before adding more cells.");
-			if(iColumnCurrent >= nColumns) throw new InvalidOperationException("Row already full. You may add a new row to fit more cells.");
+			if(iRowCurrent < 0)
+				throw new InvalidOperationException("Must add a row before adding more cells.");
+			if(iColumnCurrent >= iColumnLast)
+			{
+				++iColumnLast;
+				table.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+			}
 		}
 	}
 }
